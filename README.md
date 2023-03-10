902c78903be8429627e1b8df04d59df2d7b58ce6
Gabriel Garcia <100931438+gabrielgarcia05@users.noreply.github.com>
2023-03-10 19:04:03 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# iw-inteligencia_artificial
+# inteligencia_artificial_iw2
+
+https://gabrielgarcia05.github.io/inteligencia_artificial_iw2/
---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function troca1(){
+    window.location.href = 'index.html'
+}
+function troca2(){
+    window.location.href = 'index2.html'
+}
+function troca3(){
+    window.location.href = 'index3.html'
+}
+function troca4(){
+    window.location.href = 'index4.html'
+}
+function troca5(){
+    window.location.href = 'index5.html'
+}
